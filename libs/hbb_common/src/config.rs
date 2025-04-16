cdd3fe4b57b148df4dab1360b3eeddd1134804ae
iokapp <andm@qq.com>
2025-04-16 17:42:57 +0800

    Update config.rs-128dcom

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -83,10 +83,10 @@ lazy_static::lazy_static! {
     pub static ref APP_HOME_DIR: RwLock<String> = Default::default();
 }
 
-pub const LINK_DOCS_HOME: &str = "https://rustdesk.com/docs/en/";
-pub const LINK_DOCS_X11_REQUIRED: &str = "https://rustdesk.com/docs/en/manual/linux/#x11-required";
+pub const LINK_DOCS_HOME: &str = "http://128d.com";
+pub const LINK_DOCS_X11_REQUIRED: &str = "http://128d.com";
 pub const LINK_HEADLESS_LINUX_SUPPORT: &str =
-    "https://github.com/rustdesk/rustdesk/wiki/Headless-Linux-Support";
+    "http://128d.com";
 lazy_static::lazy_static! {
     pub static ref HELPER_URL: HashMap<&'static str, &'static str> = HashMap::from([
         ("rustdesk docs home", LINK_DOCS_HOME),
@@ -100,16 +100,16 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["t.iok.app"];
-pub const PUBLIC_RS_PUB_KEY: &str = "qq170072201";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["ddns.128d.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "128d.com";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
     _ => PUBLIC_RS_PUB_KEY,
 };
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
+pub const RENDEZVOUS_PORT: i32 = 22226;
+pub const RELAY_PORT: i32 = 22227;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
