ed607c22d7399023e18268c01a8704336aab8204
iokapp <andm@qq.com>
2025-04-16 17:45:16 +0800

    Update config.rs-128dcom

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,16 +100,16 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["t.iok.app"];
-pub const PUBLIC_RS_PUB_KEY: &str = "qq170072201";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["ddns.128d.com:"];
+pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
     _ => PUBLIC_RS_PUB_KEY,
 };
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
+pub const RENDEZVOUS_PORT: i32 = 22226;
+pub const RELAY_PORT: i32 = 22227;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
